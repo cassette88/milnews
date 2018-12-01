97892a9ba144e22cb3d1cbd8e14aa15fb9ff5a82
Dave <Dave@Daves-Mac-mini.local>
2018-12-01 14:01:30 -0500

    amazon ad test

--- client/src/Foot.js
+++ client/src/Foot.js
@@ -12,6 +12,18 @@ return (
       <p>
      Latest miilitary news sourced by Google Alerts.
       </p>
+      <script type="text/javascript">
+amzn_assoc_tracking_id = "milmerch-20";
+amzn_assoc_ad_mode = "manual";
+amzn_assoc_ad_type = "smart";
+amzn_assoc_marketplace = "amazon";
+amzn_assoc_region = "US";
+amzn_assoc_design = "enhanced_links";
+amzn_assoc_asins = "B01HNNH8TW";
+amzn_assoc_placement = "adunit";
+amzn_assoc_linkid = "15dab1eef71bdf2afa430615fec2de77";
+</script>
+<script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US"></script>
       </Col>
       {/* <Col md="6">
       <h5 className="title">Links</h5>
